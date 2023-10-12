dfcde4c111fe37e90e80e2c7d12cf1bc1da813e8
arkarsg <alxarkar@gmail.com>
2023-10-12 14:58:00 +0800

    Install material tailwind

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -1,7 +1,9 @@
-module.exports = {
+const withMT = require("@material-tailwind/react/utils/withMT");
+ 
+module.exports = withMT({
   content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
     extend: {},
   },
   plugins: [],
-};
+});
