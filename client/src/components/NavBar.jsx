21654b15aef565ef3d2925a9920c5b345330a101
arkarsg <alxarkar@gmail.com>
2023-10-13 09:48:42 +0800

    Improve HTML structure for screen readers

--- client/src/components/NavBar.jsx
+++ client/src/components/NavBar.jsx
@@ -3,10 +3,13 @@ import { Link } from "react-router-dom";
 const NavBar = () => {
   return (
     // NavBar is a landmark
-    <nav className="bg-gradient-to-b from-zinc-100 to-bg-transparent">
+    <nav
+      role="navigation"
+      className="bg-gradient-to-b from-zinc-100 to-bg-transparent"
+    >
       <div className="max-w-7xl mx-auto py-7 px-7 sm:px-4 flex justify-center">
         <div className="absolute inset-x-0 top-2 h-16 px-10">
-          <Link to="/" className="flex gap-2 items-center">
+          <Link aria-label="Home" to="/" className="flex gap-2 items-center">
             <p className="bg-clip-text bg-gradient-to-r from-fuchsia-700 to-purple-600 text-2xl font-extrabold text-transparent md:block">
               Accessible Tic Tac Toe
             </p>
