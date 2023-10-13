f4862c8b4a5a65f342c9e8c8e4cb2e308b67b234
arkarsg <alxarkar@gmail.com>
2023-10-13 10:40:36 +0800

    Add keyboard controls and labels for tictactoe

--- client/src/components/Board.jsx
+++ client/src/components/Board.jsx
@@ -4,15 +4,15 @@ const Board = ({tiles, handleTileClick}) => {
   return (
     <>
       <div className="grid grid-cols-3 mx-auto w-64 border-black border-solid border-2">
-        <Tile onClick={() => handleTileClick(0)} value={tiles[0]} />
-        <Tile onClick={() => handleTileClick(1)} value={tiles[1]} />
-        <Tile onClick={() => handleTileClick(2)} value={tiles[2]} />
-        <Tile onClick={() => handleTileClick(3)} value={tiles[3]} />
-        <Tile onClick={() => handleTileClick(4)} value={tiles[4]} />
-        <Tile onClick={() => handleTileClick(5)} value={tiles[5]} />
-        <Tile onClick={() => handleTileClick(6)} value={tiles[6]} />
-        <Tile onClick={() => handleTileClick(7)} value={tiles[7]} />
-        <Tile onClick={() => handleTileClick(8)} value={tiles[8]} />
+        <Tile onClick={() => handleTileClick(0)} value={tiles[0]} id={1}/>
+        <Tile onClick={() => handleTileClick(1)} value={tiles[1]} id={2}/>
+        <Tile onClick={() => handleTileClick(2)} value={tiles[2]} id={3}/>
+        <Tile onClick={() => handleTileClick(3)} value={tiles[3]} id={4}/>
+        <Tile onClick={() => handleTileClick(4)} value={tiles[4]} id={5}/>
+        <Tile onClick={() => handleTileClick(5)} value={tiles[5]} id={6}/>
+        <Tile onClick={() => handleTileClick(6)} value={tiles[6]} id={7}/>
+        <Tile onClick={() => handleTileClick(7)} value={tiles[7]} id={8}/>
+        <Tile onClick={() => handleTileClick(8)} value={tiles[8]} id={9}/>
       </div>
     </>
   );
--- client/src/components/Tile.jsx
+++ client/src/components/Tile.jsx
@@ -1,13 +1,14 @@
-const Tile = ({ value, onClick }) => {
+import { TILE_POSITIONS } from "../utils/GameUtils";
+
+const Tile = ({ value, onClick, id }) => {
   return (
-    <div
-      className="h-24 border-solid border-2 border-black text-center flex justify-center align-middle cursor-pointer"
-      onClick={onClick}
-    >
-      <span className="font-extrabold flex items-center justify-center text-2xl">
-        {value}
-      </span>
-    </div>
+    <button onClick={onClick} aria-label={TILE_POSITIONS[id]}>
+      <div className="h-24 border-solid border-2 border-black text-center flex justify-center align-middle cursor-pointer">
+        <span className="font-extrabold flex items-center justify-center text-2xl">
+          {value}
+        </span>
+      </div>
+    </button>
   );
 };
 
