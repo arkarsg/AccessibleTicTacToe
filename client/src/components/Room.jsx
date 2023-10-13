dc853865d61fa71f65c2815cdec77ae90361935e
arkarsg <alxarkar@gmail.com>
2023-10-13 10:05:08 +0800

    Improve Room accessibility

--- client/src/components/Room.jsx
+++ client/src/components/Room.jsx
@@ -9,14 +9,12 @@ const Room = () => {
   }
   return (
     <section>
-      <div className="mt-10 text-center py-4">
-        {/* Room information */}
-        <h2 className="text-3xl text-zinc-900 font-semibold">
-          <span className="rounded-full bg-neutral-50 px-10 py-5">
-            Playing in room {dummyRoom}
-          </span>
-        </h2>
-      </div>
+      {/* Room information */}
+      <h2 className="text-center text-3xl text-zinc-900 font-semibold">
+        <span className="rounded-full bg-neutral-50 px-10 py-5">
+          Playing in room {dummyRoom}
+        </span>
+      </h2>
       <GameBoard room={dummyRoom} />
     </section>
   );
