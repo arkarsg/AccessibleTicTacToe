934be44a177a78ae7f21c16059375fcf420b6c3f
arkarsg <alxarkar@gmail.com>
2023-10-13 11:49:31 +0800

    Set up Mongoose DB

--- server/index.js
+++ server/index.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const cors = require("cors");
 const http = require("http");
+const mongoose = require("mongoose");
 const { setupSocketServer } = require("./socketServer");
 
 require("dotenv").config({ path: "./.env" });
@@ -8,6 +9,9 @@ require("dotenv").config({ path: "./.env" });
 const PORT = process.env.PORT || 8800;
 const app = express();
 app.use(cors());
+app.use(express.json());
+
+mongoose.connect(process.env.MONGO_URL).catch(error => console.error(error));
 
 const server = http.createServer(app);
 setupSocketServer(server);
